03b3afc3657ff37b0a341baf6479b7dc470862e8
mtuonglatui <tuongphan2004@gmail.com>
2023-12-15 00:22:43 +0700

    update pro-detail/ feedback-comment

---
+++ model_DAO/feedback.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'pdo.php';
+
+function feedback_list($id){
+      $sql="SELECT * FROM BinhLuan bl INNER JOIN KhachHang kh ON bl.MaKhachHang=kh.MaKhachHang WHERE MaSanPham=?";
+      return pdo_query($sql,$id);
+      } 
+   function feedback_add($idKH,$feedback,$id) {
+      date_default_timezone_set("Asia/Ho_Chi_Minh");
+      $sql="INSERT INTO BinhLuan(MaKhachHang,NoiDung,MaSanPham,`NgayBinhLuan`) VALUES (?,?,?,?)";
+      return pdo_execute($sql,$idKH,$feedback,$id,Date("Y-m-d H-i-s"));
+   }    
+?>
