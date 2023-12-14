77f91cb16b5d21707f0bb68790b513737a732308
mtuonglatui <tuongphan2004@gmail.com>
2023-12-15 00:13:07 +0700

    update product-detail/feedback-comment

--- site/controller/product.php
+++ site/controller/product.php
@@ -1,20 +1,22 @@
 <?php
-// include_once '../model_DAO/product.php';
-// extract($_REQUEST);
-// include_once './view/template_header.php';
+    include_once '../model_DAO/product.php';
+    include_once '../model_DAO/feedback.php';
+    extract($_REQUEST);
+    if(isset($act)){
+            switch ($act){
+                case 'detail':
+                    $product_detail=product_one($id);
+                    $feedback_list=feedback_list($id);
+                    if (isset($feedback_submit)) {
+                        feedback_add($_SESSION['user']['MaKhachHang'],$feedback,$id);
+                        header ('location: ?mod=product&act=detail&id= '.$id);
+                    }
+                    include_once 'view/template_header.php';
+                    include_once 'view/page_product_detail.php';
+                    include_once 'view/page_feedback.php';
+                    include_once 'view/template_footer.php';
+                    break;
+            }
+    }
 
-// if(isset($act)){
-//     switch($act){
-//         case "detail":
-//             include './view/page_product.php';
-//             include './view/template_footer.php';
-//             if(isset($_GET['id']) && $_GET['id']>0){
-//                 $id = $_GET['id'];
-//                 $detail = product_detail_by_id($id);
-//             }else {
-//                 $id = 0;
-//             }
-//             break;
-//     }
-// }
 ?>
